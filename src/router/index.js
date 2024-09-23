b434ae7d3340fb45b33634358aa9a422dc1a05e7
ChristianHopf <cehopf@gmail.com>
2024-09-23 16:13:36 -0400

    Routes, views for home cards

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import SecurityGuardsView from "../view/SecurityGuardsView.vue";
+// import ProjectIcarusView from "../ProjectIcarusView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/mission",
+      name: "mission",
+      component: HomeView,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: HomeView,
+    },
+    {
+      path: "/security-guards",
+      name: "security-guards",
+      component: SecurityGuardsView,
+    },
+    {
+      path: "/project-icarus",
+      name: "project-icarus",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/ProjectIcarusView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/SecurityGuardsView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <section class="container">
+            <div id="content">
+            </div>
+        </section>
+    </main>
+</template>
+
+<style>
+.container {
+    position: relative;
+    width: 100%;
+    margin: 0 auto;
+}
+</style>
